3298777b37f817ea3cb9215588c26752d9cdae5f
Min <sou05094@gmail.com>
2023-04-06 09:20:14 +0900

    수정

--- homework1.java
+++ homework1.java
@@ -1,21 +1,23 @@
-public class MatrixMultiplication {
+package chapter4;
+
+public class mission2 {
     public static void main(String[] args) {
-        int[][] matrix1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
-        int[][] matrix2 = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
-        int[][] result = new int[4][4]; // 곱셈 결과 행렬
+        int[][] matrix1 = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+        int[][] matrix2 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
+        int[][] result = new int[3][3]; // 곱셈 결과 행렬
 
         // 행렬 곱셈 연산
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                for (int k = 0; k < 3; k++) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 4; k++) {
                     result[i][j] += matrix1[i][k] * matrix2[k][j];
                 }
             }
         }
 
         // 곱셈 결과 출력
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j <3 ; j++) {
                 System.out.print(result[i][j] + " ");
             }
             System.out.println();
